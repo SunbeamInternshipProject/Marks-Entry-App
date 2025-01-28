88bbee749d4ed2ba6761db8b2281c9befc3eadd7
Shreya Jagdish Patil <shreyacse42@gmail.com>
2025-01-28 13:11:06 +0530

    added Student Login Endpoint

--- BackEnd/routes/student.js
+++ BackEnd/routes/student.js
@@ -4,6 +4,7 @@ const db = require("../db");
 const utils = require("../utils");
 const config = require("../config");
 const crypto = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", (request, response) => {
     console.log("*: INside student register");
@@ -37,6 +38,61 @@ router.post("/register", (request, response) => {
       }
     });
   });
+
+// LOGIN API
+router.post("/login", (request, response) => {
+  // Destructuring email and password from the request body
+  const { email, password } = request.body;
+
+  console.log("email + password ", email, password);
+
+  const statement = `SELECT student_id, roll_number, student_name, email 
+      FROM students 
+      WHERE email = ? AND password = ?`;
+
+  console.log("statement ", statement);
+
+  // Encrypting the provided
+  const encryptedPassword = String(crypto.SHA256(password));
+
+  // Executing the SQL query with user-provided email and encrypted password
+  db.execute(statement, [email, encryptedPassword], (error, users) => {
+    if (error) {
+      response.send(utils.createErrorResponse(error));
+    } else {
+      if (users.length === 0) {
+        response.send(utils.createErrorResponse("user not found!"));
+      } else {
+        const student = users[0];
+        console.log("After response from mysql: ", student);
+
+        // Creating a payload with user information for JWT token
+        const payload = {
+          student_id: student["student_id"],
+          email: student["email"],
+          roll_number: student["roll_number"],
+          student_name: student["student_name"],
+          role: "student",
+        };
+
+        console.log("Check Student payload ", payload);
+
+        // Generating a JWT token with the payload and a secret key
+        const token = jwt.sign(payload, config.SECRET_KEY, { expiresIn: "1h" });
+        console.log("JWT Token is : "+token);
+        response.send(
+          utils.createSuccessResponse({
+            token,
+            // username: user["username"], or
+            student_name: student.student_name,
+          })
+        );
+      }
+    }
+  });
+});
+
+
 
 
 
